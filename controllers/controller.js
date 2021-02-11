cad35f18b8d210feb8be7592af858a8c20e3222f
Glitch (hello-sqlite) <none>
2021-02-11 10:25:16 +0000

    🛩🍫 Checkpoint ./views/index.ejs:21051661/485 ./views/layout.ejs:21051661/283 ./package.json:21051661/1758 ./controllers/controller.js:21051661/222

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,10 @@
 //とりあえずのホーム画面
 exports.cesiumInitialControl = (req, res) =>{
-    const message = "Hello World!";
+    
     let param = {
-      title: "バスローケーションデータ連携(GTFS-RT)",
-      description: "東京都交通局バスロケーション情報を30秒に1回更新でプロットするサンプルです",
-      message:"Hello World!"
+      title: "バーチャルフリーマーケット",
+      description: "バーチャルフリーマーケットサンプルです",
+      message:"サンプル"
     }
     res.render("index", param);
 }
