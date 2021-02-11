9f987cd11f5ded3310a7198ebb68fac37266ff15
Glitch (hello-sqlite) <none>
2021-02-11 13:52:06 +0000

    👠⛱ Checkpoint ./controllers/controller.js:21051661/307

--- controllers/controller.js
+++ controllers/controller.js
@@ -8,3 +8,14 @@ exports.cesiumInitialControl = (req, res) =>{
     }
     res.render("index", param);
 }
+
+//イベント登録
+exports.cesiumInitialControl = (req, res) =>{
+    
+    let param = {
+      title: "イベント登録",
+      description: "バーチャルフリーマーケットサンプルです",
+      message:"サンプル"
+    }
+    res.render("index", param);
+}
